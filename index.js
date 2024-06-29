aa2addeba09992de38f6bc7351b7854b60b3bb8f
Yahya Mohamed <171671702+yahya-mohamed-1@users.noreply.github.com>
2024-06-29 18:56:12 +0300

    Can now close modal by several ways

---
+++ index.js
@@ -0,0 +1,31 @@
+const showModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+
+// Add event to all buttons
+for (i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
+
+// add event to paragraph to close the modal
+const paragraph = document.querySelector("p");
+paragraph.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+// add event to close (x) button to close the modal
+closeModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+// add event to modal to close the modal
+modal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
